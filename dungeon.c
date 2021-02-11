a6db11cb813b540e708f16961c2b817c389973b9
Student <kshecterle@gmail.com>
2021-02-10 20:58:27 -0600

    yo pt.2

--- dungeon.c
+++ dungeon.c
@@ -7,9 +7,9 @@ char dung[80][21];
 int main(int argc, char *argv[]){
 srand(time(NULL));
 
-makeRooms();
+makeMap();
 }
-int makeRooms()
+int makeMap()
 {
     //return 1 if true 0 if false for checking intersections
     struct room{
@@ -40,16 +40,107 @@ int makeRooms()
     }
     for (int i = 0; i < 6; i++)
     {
+        int temp = 1;
         fillArrayRoom(rooms[i].x, rooms[i].y, rooms[i].x+rooms[i].x_size, rooms[i].y+rooms[i].y_size);
+	if (i == 5){
+	  temp = 0;
+	}
+	fillArrayCorridor(rooms[i].x, rooms [i].y, room[temp].x, room[temp].y);
+	temp ++;
     }
     //put rooms in array here
     return 1; 
 
 }
+
 void fillArrayRoom(int topX, int topY,int botX,int botY){
     for(topX; topX<=botX;topX++){
         for(topY; topY<=botY;topY++){
             dung[topX][topY] = '.';
         }
     }
+}
+
+void fillArrayCorridor(int rm1x, int rm1y, int rm2x, int rm2y)
+{
+  int tempx =0;
+  int tempy =0;
+  bool right;
+  bool up;
+
+  if(rm1y > rm2y){
+    tempy = rm1y - rm2y;
+    up = true;
+  } if(rm1y < rm2y){
+    tempy = rm2y - rm1y;
+    up = false;
+  } if(rm1y == rm2y){
+    tempy = 0;
+    up = true;
+  }
+
+  if(rm1x > rm2x){
+    tempx = rm1x - rm2x;
+    right = true;
+  } if(rm1y < rm2y){
+    tempx = rm2x - rm1x;
+    right = false;
+  } if(rm1y == rm2y){
+    tempx = 0;
+    right = true;
+  }
+
+  if (right){
+    if (up){
+      for(tempx; tempx > 0; tempx--){
+	for (tempy; tempy > 0; tempy--){
+	  if(dung[rm1x + tempx][rm1y + tempy] != ' '){
+	  } else {
+	    dung[rm1x + tempx][rm1y + tempy] = '#';
+	  }
+	}
+  }
+    } else {
+      for(tempx; tempx > 0; tempx--){
+	for (tempy; tempy > 0; tempy--){
+	  if(dung[rm1x + tempx][rm1y - tempy] != ' '){
+	  } else {
+	    dung[rm1x + tempx][rm1y - tempy] = '#';
+	  }
+	}
+      }
+    }
+  } else {
+    if(up){
+      for(tempx; tempx > 0; tempx--){
+	for (tempy; tempy > 0; tempy--){
+	  if(dung[rm1x - tempx][rm1y + tempy] != ' '){
+	  } else {
+	    dung[rm1x - tempx][rm1y + tempy] = '#';
+	  }
+	}
+      }
+      
+    } else {
+      for(tempx; tempx > 0; tempx--){
+	for (tempy; tempy > 0; tempy--){
+	  if(dung[rm1x - tempx][rm1y - tempy] != ' '){
+	  } else {
+	    dung[rm1x - tempx][rm1y - tempy] = '#';
+	  }
+	}
+      }
+    }
+  }
+
+
+  for(tempx; tempx > 0; tempx--){
+    for (tempy; tempy > 0; tempy--){
+      if(dung[rm1x - tempx][rm1y - tempy] != ' '){
+      } else {
+	dung[rm1x - tempx][rm1y - tempy] = '#';
+      }
+    }
+  }
+  
 }
